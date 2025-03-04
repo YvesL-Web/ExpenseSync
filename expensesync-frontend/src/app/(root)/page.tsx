c1c798f8dea1fe7860f429c039d955ea76379ada
landry <yvesengoulou@yahoo.fr>
2025-03-04 23:24:56 +0100

    feat: layout implemented and root page implemented

--- expensesync-frontend/src/app/(root)/page.tsx
+++ expensesync-frontend/src/app/(root)/page.tsx
@@ -1,6 +1,26 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const page = () => {
+  const loggedIn = { firstName: "Yves" };
   return (
-    <div>page</div>
-  )
-}
-export default page
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={5000.50}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+export default page;
